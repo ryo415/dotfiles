4f46a2f76db54be759de280b25b4a55dba827b1a
ryohei.kikuchi <black_in7@hotmail.co.jp>
2019-10-17 13:33:50 +0900

    パス修正

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.cache/dein
 cd ~/.cache/dein
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
 
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
