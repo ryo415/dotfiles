a50df969b442dd252ac5fb0df179d820fcbdc6a2
Ryohei Kikuchi <ryohei.kikuchi@gree.net>
2019-10-17 10:30:17 +0900

    NeoBundleインストール追加

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+sh ./install.sh
+
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/.bash_profile $HOME/.bash_profile
