32ad1673e9c50cab2f259b49519a2fbf52f12b48
ryo415 <black_in7@hotmail.co.jp>
2020-02-01 17:34:49 +0900

    update

--- .bash_profile
+++ .bash_profile
@@ -2,17 +2,6 @@ if [ -f ~/.bashrc ]; then
     . ~/.bashrc
 fi
 
-# pyenv
-export PYENV_ROOT=/usr/local/var/pyenv
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-
-# rbenv
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
 # alias
 alias la='ls -a'
 alias ll='ls -l'
-
-export PATH="$HOME/.cargo/bin:$PATH"
--- dotfile_installer.sh
+++ dotfile_installer.sh
@@ -2,12 +2,12 @@
 
 function helpmsg() {
   command echo "Usage: $0 [link | update]" 0>&2
-	command echo ""
+  command echo ""
 }
 
 function main() {
-	local link="false"
-	local update="false"
+  local link="false"
+  local update="false"
 
   if [ $# = 0 ];then
     link="true"
@@ -16,38 +16,37 @@ function main() {
   while [ $# -gt 0 ];do
     case ${1} in
       --help|-h)
-			  helpmsg
-				exit 1
-				;;
-			link)
-				link="true"
-				;;
-			update)
-				update="true"
-				;;
-			*)
-			  echo "[ERROR] Invalied arguments '${1}'"
-				helpmsg
-				exit 1
-				;;
-		esac
-		shift
+		helpmsg
+		exit 1
+		;;
+	  link)
+		link="true"
+		;;
+	  update)
+		update="true"
+		;;
+	  *)
+		echo "[ERROR] Invalied arguments '${1}'"
+		helpmsg
+		exit 1
+		;;
+	esac
+	shift
   done
 
-	if [[ "$link" = true ]];then
- 	  ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+  if [[ "$link" = true ]];then
+ 	ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
     ln -s $HOME/dotfiles/.bash_profile $HOME/.bash_profile
-		ln -s $HOME/dotfiles/.vim $HOME/.vim
+	ln -s $HOME/dotfiles/.vim $HOME/.vim
   fi
-	if [[ "$update" = true ]];then
-		unlink $HOME/.vimrc
-		unlink $HOME/.bash_profile
-		unlink $HOME/.vim
-	  ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+  if [[ "$update" = true ]];then
+	unlink $HOME/.vimrc
+	unlink $HOME/.bash_profile
+    unlink $HOME/.vim
+	ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
     ln -s $HOME/dotfiles/.bash_profile $HOME/.bash_profile
-		ln -s $HOME/dotfiles/.vim $HOME/.vim
-	fi
-
+    ln -s $HOME/dotfiles/.vim $HOME/.vim
+  fi
 }
 
 main "$@"
