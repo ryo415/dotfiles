f95cef86fc5ae730cb1ac03fbfc68e6f7e963b33
Ryohei Kikuchi <ryohei.kikuchi@gree.net>
2020-02-17 18:22:17 +0900

    tmux起動時のシェルをzshに変更

--- dotfile_installer.sh
+++ dotfile_installer.sh
@@ -11,41 +11,44 @@ function main() {
 
   if [ $# = 0 ];then
     link="true"
-	fi
+  fi
 
   while [ $# -gt 0 ];do
     case ${1} in
       --help|-h)
-		helpmsg
-		exit 1
-		;;
-	  link)
-		link="true"
-		;;
-	  update)
-		update="true"
-		;;
-	  *)
-		echo "[ERROR] Invalied arguments '${1}'"
-		helpmsg
-		exit 1
-		;;
+        helpmsg
+        exit 1
+        ;;
+      link)
+        link="true"
+        ;;
+      update)
+        update="true"
+        ;;
+      *)
+        echo "[ERROR] Invalied arguments '${1}'"
+        helpmsg
+        exit 1
+        ;;
 	esac
 	shift
   done
 
   if [[ "$link" = true ]];then
- 	  ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
-    ln -s $HOME/dotfiles/.zprofile $HOME/.zprofile
-	  ln -s $HOME/dotfiles/.vim $HOME/.vim
-  fi
-  if [[ "$update" = true ]];then
-	  unlink $HOME/.vimrc
-	  unlink $HOME/.zprofile
-    unlink $HOME/.vim
- 	  ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+    ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
     ln -s $HOME/dotfiles/.zprofile $HOME/.zprofile
     ln -s $HOME/dotfiles/.vim $HOME/.vim
+    ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+  fi
+  if [[ "$update" = true ]];then
+    unlink $HOME/.vimrc
+    unlink $HOME/.zprofile
+    unlink $HOME/.vim
+    unlink $HOME/.tmux.conf
+    ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+    ln -s $HOME/dotfiles/.zprofile $HOME/.zprofile
+    ln -s $HOME/dotfiles/.vim $HOME/.vim
+    ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
   fi
 }
 
