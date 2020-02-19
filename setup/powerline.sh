ed28d6b5a8810bb654877b1da4161f68feb65c35
Ryohei Kikuchi <ryohei.kikuchi@gree.net>
2020-02-19 10:20:04 +0900

    追加

--- setup/powerline.sh
+++ setup/powerline.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
 
-pip3 install powerline-status
 cd /tmp
+git clone https://github.com/powerline/powerline.git
+cd powerline
+python3 setup.py install
+cd ..
 git clone https://github.com/powerline/fonts.git
 cd fonts
 ./install.sh
