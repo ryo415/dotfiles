8e4bdf3af10bc4b38909c9cbd3203fe7cf34194b
Ryohei Kikuchi <ryohei.kikuchi@gree.net>
2020-02-20 17:15:09 +0900

    .zshrc追加

---
+++ .zshrc
@@ -0,0 +1,12 @@
+# powerline
+# powerline-daemon -q
+# . /usr/local/lib/python3.7/site-packages/powerline/bindings/zsh/powerline.zsh
+
+# rbenv
+# export PATH="$HOME/.rbenv/bin:$PATH"
+# eval "$(rbenv init -)"
+
+# 補完
+# fpath=($(brew --prefix)/share/zsh/site-functions $fpath)
+# autoload -U compinit
+# compinit -u
--- dotfile_installer.sh
+++ dotfile_installer.sh
@@ -10,7 +10,7 @@ function main() {
   local update="false"
 
   if [ $# = 0 ];then
-    link="true"
+    update="true"
   fi
 
   while [ $# -gt 0 ];do
@@ -37,18 +37,22 @@ function main() {
   if [[ "$link" = true ]];then
     ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
     ln -s $HOME/dotfiles/.zprofile $HOME/.zprofile
+		ln -s $HOME/dotfiles/.zsh $HOME/.zsh
     ln -s $HOME/dotfiles/.vim $HOME/.vim
     ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+		cp $HOME/dotfiles/.zshrc $HOME/.zshrc
   fi
   if [[ "$update" = true ]];then
     unlink $HOME/.vimrc
     unlink $HOME/.zprofile
     unlink $HOME/.vim
     unlink $HOME/.tmux.conf
+		unlink $HOME/.zsh
     ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
     ln -s $HOME/dotfiles/.zprofile $HOME/.zprofile
     ln -s $HOME/dotfiles/.vim $HOME/.vim
     ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+		ln -s $HOME/dotfiles/.zsh $HOME/.zsh
   fi
 }
 
