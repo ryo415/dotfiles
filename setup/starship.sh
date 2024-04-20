9ffc53e02bd9600967670e4bab8ee30ef03d00e3
ryo415 <black_in7@hotmail.co.jp>
2024-04-20 22:22:21 +0900

    Create dir if .config not exists

--- setup/starship.sh
+++ setup/starship.sh
@@ -2,4 +2,9 @@
 
 curl -fsSL https://starship.rs/install.sh | bash
 
+
+if [ ! -e "$HOME/.config" ]; then
+  mkdir $HOME/.config
+fi
+
 ln -s $HOME/dotfiles/.config/starship.toml $HOME/.config/starship.toml
