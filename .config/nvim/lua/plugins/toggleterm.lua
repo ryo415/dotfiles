63750b0270018ea5de4fa453134f54ae64e1bf5f
ryo415 <black_in7@hotmail.co.jp>
2026-03-08 04:54:45 +0900

    Add nvim toggleterm plugin.

---
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,20 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    size = 100,
+    open_mapping = [[<c-t>]],
+    hide_numbers = true,
+    shade_filetypes = {},
+    shade_terminals = true,
+    shading_factor = 2,
+    start_in_insert = true,
+    float_opts = {
+      border = "curved",
+    },
+    insert_mappings = true,
+    persist_size = true,
+    direction = 'float',
+    close_on_exit = true,
+  },
+}
