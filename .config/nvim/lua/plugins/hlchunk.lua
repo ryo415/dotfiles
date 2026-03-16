d1d67ba1dc42fa9c6cab5c48585fedc170c16b51
ryo415 <black_in7@hotmail.co.jp>
2026-03-17 02:42:34 +0900

    Update hlchunk settings

--- .config/nvim/lua/plugins/hlchunk.lua
+++ .config/nvim/lua/plugins/hlchunk.lua
@@ -5,12 +5,19 @@ return {
     require("hlchunk").setup({
       chunk = {
         enable = true,
+        duration = 0,
+        delay = 0,
       },
       indent = {
         enable = true,
+        deley = 0,
+        ahead_lines = 20,
       },
       blank = {
-        enable = true,
+        enable = false,
+      },
+      line_num = {
+        enable = false,
       },
     })
   end
